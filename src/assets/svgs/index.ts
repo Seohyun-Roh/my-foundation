b22dd11bd94e7635c58a5a43f68c4958428945cb
Seohyun-Roh <radult951@gmail.com>
2022-06-15 12:40:48 +0900

    Add DropDown component

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,2 @@
 export { ReactComponent as LogoImage } from './logo.svg'
+export { ReactComponent as DownArrow } from './downArrow.svg'
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,20 @@
 import styles from './home.module.scss'
 
+import DropDown from 'components/DropDown'
+import { useState } from 'react'
+
+const DROPDOWN_CATEGORIES = ['전체', '음식점', '관광명소', '카페', '숙박', '문화시설', '기타']
+
 const Home = () => {
-  return <div className={styles.container}>Home</div>
+  const [currentCategory, setCurrentCategory] = useState('전체')
+
+  return (
+    <div className={styles.container}>
+      <DropDown selectList={DROPDOWN_CATEGORIES} setCurrentSelect={setCurrentCategory} size='small'>
+        {currentCategory}
+      </DropDown>
+    </div>
+  )
 }
 
 export default Home
