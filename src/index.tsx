8e77fbf1476a34d117ba16a6a05fe34eccccd7d2
Seohyun-Roh <radult951@gmail.com>
2022-09-18 23:39:36 +0900

    add i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next'
 import { RecoilRoot } from 'recoil'
 
+import i18n from 'utils/locale'
 import reportWebVitals from './reportWebVitals'
 import './styles/index.scss'
 import Routes from './routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <RecoilRoot>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </React.StrictMode>
-  </RecoilRoot>
+  <I18nextProvider i18n={i18n}>
+    <RecoilRoot>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </React.StrictMode>
+    </RecoilRoot>
+  </I18nextProvider>
 )
 
 // If you want to start measuring performance in your app, pass a function
