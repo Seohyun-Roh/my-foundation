47c4a3c2a3f576dccd3805c759cdb523d920f55d
Seohyun-Roh <radult951@gmail.com>
2022-11-29 05:02:35 +0900

    add storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import '../styles/index.scss'
+import { Button } from 'components'
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const ButtonTemplate = Template.bind({})
+
+ButtonTemplate.args = {
+  primary: true,
+  size: 'large',
+  children: 'Button',
+}
