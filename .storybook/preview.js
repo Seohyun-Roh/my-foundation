962e63b92f7b2351a7cf367c99519b983a322dd7
Seohyun-Roh <radult951@gmail.com>
2022-12-04 21:09:03 +0900

    add storybook dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import { themes } from '@storybook/theming'
+import { useDarkMode } from 'storybook-dark-mode'
+
+export const decorators = [
+  (Story) => {
+    document.documentElement.setAttribute('color-theme', useDarkMode() ? 'dark' : 'light')
+
+    return <Story />
+  },
+]
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
+  darkMode: {
+    dark: themes.dark,
+    light: themes.light,
+    current: 'light',
+  },
+}
